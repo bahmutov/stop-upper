ac362446b77dd39eeb64364d7b13973a7282376f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-09-24 12:23:59 -0400

    initial commit

---
+++ src/stop-upper-spec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* eslint-env mocha */
+const stopUpper = require('.')
+
+describe('stop-upper', () => {
+  it('write this test', () => {
+    console.assert(stopUpper, 'should export something')
+  })
+})
